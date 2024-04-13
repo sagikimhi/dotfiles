bee8af005bf4e749671c723ee24c7e22eff221fa
SagiKimhi <sagi.kim5@gmail.com>
2024-04-13 11:55:18 -0400

    update nvim config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,38 +1,192 @@
 local lsp = require("lsp-zero")
 
-lsp.preset("recommended")
-
-lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-})
-
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
-
+-- Set Up Autocompletion
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<Up>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<Down>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<Right>'] = cmp.mapping.confirm({
-      behavior=cmp.ConfirmBehavior.Replace,
-      select = true
-  }),
-  ['<Enter>'] = cmp.mapping.confirm({
-      behavior=cmp.ConfirmBehavior.Insert,
-      select = true
-  }),
-  ['<Left>']    = cmp.mapping.abort(),
-  ["<C-Space>"] = cmp.mapping.complete(),
+    ['Tab']       = nil,
+    ['S-Tab']     = nil,
+    ['<Up>']      = cmp.mapping.select_prev_item(cmp_select),
+    ['<Down>']    = cmp.mapping.select_next_item(cmp_select),
+    ['<Left>']    = cmp.mapping.abort(),
+    ['<Right>']   = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    ['<Enter>']   = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+-- ===============================================================================
+-- Set LSP Keymaps
+-- ===============================================================================
+--
+-- Default Keymaps:
+-- ----------------
+-- K:           Display Hover information about the symbol under cursor. (`vim.lsp.buf.hover()`)
+-- gd:          Jump to definition of symbol under cursor (`vim.lsp.buf.definition()`)
+-- gD:          Jump to declaration of symbol under cursor (`vim.lsp.buf.definition()`)
+-- go:          Jump to type-definition of symbol under cursor (`vim.lsp.buf.type_definition()`)
+-- gi:          List all implementations for symbol under cursor (`vim.lsp.buf.implementation()`)
+-- gr:          List all references to symbol under cursor (`vim.lsp.buf.references()`)
+-- gs:          Display signature information about symbol under cursor (`vim.lsp.buf.signature_help()`)
+-- gl:          Display Diagnostics in a floating window (`vim.diagnostic.open_float()`)
+-- ]d:          Move to next diagnostic in current buffer (`vim.diagnostic.goto_next()`)
+-- [d:          Move to previous diagnostic in current buffer (`vim.diagnostic.goto_prev()`)
+-- <F2>:        Rename all references to symbol under cursor (`vim.lsp.buf.rename()`)
+-- <F3>:        Format code in current buffer (`vim.lsp.buf.format()`)
+-- <F4>:        Select a code action available at current cursor position (`vim.lsp.buf.code_action()`)
+--
+-- Additional Keymaps - Insert Mode:
+-- ---------------------------------
+-- Ctrl-h:      Display signature information about symbol under cursor (`vim.lsp.buf.signature_help()`)
+--
+-- Additional Keymaps - Normal Mode:
+-- ---------------------------------
+-- Leader-f:    Format code in current buffer (`vim.lsp.buf.format()`)
+-- Leader-gd:   Jump to declaration of symbol under cursor (`vim.lsp.buf.definition()`)
 
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr }
+
+    -- Defaults - w/o overriding existing
+    lsp.defaults.keymaps({ buffer = bufnr })
+
+    -- Insert mode
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, { buffer = bufnr, remap = true })
+
+    -- Normal mode
+    vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
+    vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", opts)
+    vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set('n', '<leader>gd', function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "<leader>vs", "<cmd> Telescope lsp_document_symbols<cr>", opts)
+    vim.keymap.set("n", "<leader>ws", "<cmd> Telescope lsp_workspace_symbols<cr>", opts)
+end)
+
+-- ===============================================================================
+
+
+lsp.ensure_installed({
+    -- Bash
+    "bashls",
+
+    -- C
+    "clangd",
+
+    -- Cmake
+    "cmake",
+
+    -- Docker
+    "dockerls",
+
+    -- Docker Compose
+    "docker_compose_language_service",
+
+    -- Rust
+    'rust_analyzer',
+
+    -- Go
+    -- "gopls",
+
+    -- Python
+    "ruff",
+    "ruff_lsp",
+
+    -- Emmet
+    "emmet_ls",
+
+    -- Markdown
+    -- "marksman",
+    "remark_ls",
+
+    -- HTML
+    "html",
+
+    -- CSS
+    "cssls",
+
+    -- Tailwind CSS
+    "tailwindcss",
+
+    -- Typescript/Javascript
+    'tsserver',
+
+    -- Json
+    "jsonls",
+
+    -- TOML
+    "taplo",
+
+    -- Yaml
+    "yamlls",
+
+    -- Lua
+    "lua_ls",
+
+    -- Vim
+    "vimls",
+
+    -- Arduino
+    -- "arduino_language_server",
+
+    -- Assembly
+    -- "asm_lsp",
+
+    -- Autoconf/Automake
+    -- "autotools_ls",
+
+    -- Buf
+    -- "bufls",
+
+    -- Java
+    -- "jdtls",
+
+    -- Kotlin
+    -- "kotlin-language_server",
+
+    -- Latex
+    -- "ltex",
+    -- "texlab",
+
+    -- Ocaml
+    -- "ocamllsp",
+
+    -- Perl
+    -- "perlnavigator",
+
+    -- Powershell
+    -- "powershell_es",
+
+    -- R
+    -- "r_language_server",
+
+    -- Ruby
+    -- "ruby_ls",
+
+    -- SQL
+    -- "sqls",
+    -- "sqlls",
+
+    -- Jinja
+    -- "jinja_lsp",
+
+    -- LuaU
+    -- "luau_lsp",
+
+    -- Thrift
+    -- "thriftls",
+
+    -- Sphinx (Python Doc)
+    -- "esbonio",
+
+    -- SystemVerilog
+    -- "svls",
+    -- "verible",
+    -- "svlangserver",
+
+    -- Zig
+    -- "zls",
 })
 
 lsp.set_preferences({
@@ -45,27 +199,9 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set('n', '<leader>gd', function() vim.lsp.buf.declaration() end, opts)
-  vim.keymap.set('n', '<leader>gi', function() vim.lsp.buf.implementation() end, opts)
-  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "<leader>vs", function() vim.lsp.buf.document_symbol() end, opts)
-  vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-  vim.keymap.set("n", "<leader>]", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "<leader>[", function() vim.diagnostic.goto_prev() end, opts)
-end)
-
-lsp.setup()
-
 vim.diagnostic.config({
     virtual_text = true
 })
 
+lsp.setup_nvim_cmp({ mapping = cmp_mappings })
+lsp.setup()
--- nvim/lua/my_config/packer.lua
+++ nvim/lua/my_config/packer.lua
@@ -64,11 +64,11 @@ return require('packer').startup(function(use)
 
             -- Autocompletion
             {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
             {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-buffer'},
             {'hrsh7th/cmp-nvim-lua'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'saadparwaiz1/cmp_luasnip'},
 
             -- Snippets
             {'L3MON4D3/LuaSnip'},
