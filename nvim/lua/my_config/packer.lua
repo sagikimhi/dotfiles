0ce774859606b95ff3b6307a5c7a7a3935e0cba0
SagiKimhi <sagi.kim5@gmail.com>
2024-07-21 13:49:39 +0300

    added remote nvim extension

--- nvim/lua/my_config/packer.lua
+++ nvim/lua/my_config/packer.lua
@@ -17,19 +17,56 @@ return require('packer').startup(function(use)
     use("christoomey/vim-tmux-navigator")
     use("eandrju/cellular-automaton.nvim")
 
-    use({'rose-pine/neovim',
+    use({
+        'rose-pine/neovim',
         as = 'rose-pine',
         config = function()
             vim.cmd('colorscheme rose-pine')
         end
     })
-
-    use({'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+    use({
+        "amitds1997/remote-nvim.nvim",
+        version = "*",                       -- Pin to GitHub releases
+        requires = {
+            "nvim-lua/plenary.nvim",         -- For standard functions
+            "MunifTanjim/nui.nvim",          -- To build the plugin UI
+            "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+        },
+        config = function()
+            require('remote-nvim').setup({
+                client_callback = function(port, workspace_config)
+                    local cmd = ("wezterm cli set-tab-title --pane-id $(wezterm cli spawn nvim --server localhost:%s --remote-ui) %s")
+                    :format(
+                        port,
+                        ("'Remote: %s'"):format(workspace_config.host)
+                    )
+                    if vim.env.TERM == "xterm-kitty" then
+                        cmd = ("kitty -e nvim --server localhost:%s --remote-ui"):format(port)
+                    end
+                    vim.fn.jobstart(cmd, {
+                        detach = true,
+                        on_exit = function(job_id, exit_code, event_type)
+                            -- This function will be called when the job exits
+                            print("Client", job_id, "exited with code", exit_code, "Event type:", event_type)
+                        end,
+                    })
+                end,
+                offline_mode = {
+                    enabled = true,
+                    no_github = false,
+                },
+            })
+        end
     })
 
-    use({"folke/trouble.nvim",
+    use({
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    })
+
+    use({
+        "folke/trouble.nvim",
         config = function()
             require("trouble").setup({
                 icons = false,
@@ -40,7 +77,8 @@ return require('packer').startup(function(use)
         end
     })
 
-    use({'nvim-treesitter/nvim-treesitter',
+    use({
+        'nvim-treesitter/nvim-treesitter',
         run = function()
             local ts_update = ({
                 require('nvim-treesitter.install').update(
@@ -53,27 +91,26 @@ return require('packer').startup(function(use)
     use('nvim-treesitter/playground')
     use("nvim-treesitter/nvim-treesitter-context")
 
-    use({'VonHeikemen/lsp-zero.nvim',
+    use({
+        'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-path'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-nvim-lua'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'saadparwaiz1/cmp_luasnip'},
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'saadparwaiz1/cmp_luasnip' },
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
         }
     })
-
 end)
-
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -134,6 +134,11 @@ _G.packer_plugins = {
     path = "/home/skimhi/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  ["nui.nvim"] = {
+    loaded = true,
+    path = "/home/skimhi/.local/share/nvim/site/pack/packer/start/nui.nvim",
+    url = "https://github.com/MunifTanjim/nui.nvim"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/home/skimhi/.local/share/nvim/site/pack/packer/start/nvim-cmp",
@@ -168,6 +173,12 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/skimhi/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["remote-nvim.nvim"] = {
+    config = { "\27LJ\2\nU\0\3\v\0\4\0\t6\3\0\0'\5\1\0\18\6\0\0'\a\2\0\18\b\1\0'\t\3\0\18\n\2\0B\3\a\1K\0\1\0\16Event type:\21exited with code\vClient\nprintä\2\1\2\n\0\14\0\30'\2\0\0\18\4\2\0009\2\1\2\18\5\0\0'\6\2\0\18\b\6\0009\6\1\0069\t\3\1B\6\3\0A\2\2\0026\3\4\0009\3\5\0039\3\6\3\a\3\a\0X\3\6€'\3\b\0\18\5\3\0009\3\1\3\18\6\0\0B\3\3\2\18\2\3\0006\3\4\0009\3\t\0039\3\n\3\18\5\2\0005\6\v\0003\a\f\0=\a\r\6B\3\3\1K\0\1\0\fon_exit\0\1\0\1\vdetach\2\rjobstart\afn4kitty -e nvim --server localhost:%s --remote-ui\16xterm-kitty\tTERM\benv\bvim\thost\17'Remote: %s'\vformatgwezterm cli set-tab-title --pane-id $(wezterm cli spawn nvim --server localhost:%s --remote-ui) %s…\1\1\0\4\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0003\3\3\0=\3\5\0025\3\6\0=\3\a\2B\0\2\1K\0\1\0\17offline_mode\1\0\2\fenabled\2\14no_github\1\20client_callback\1\0\0\0\nsetup\16remote-nvim\frequire\0" },
+    loaded = true,
+    path = "/home/skimhi/.local/share/nvim/site/pack/packer/start/remote-nvim.nvim",
+    url = "https://github.com/amitds1997/remote-nvim.nvim"
   },
   ["rose-pine"] = {
     config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
@@ -232,6 +243,10 @@ time([[Config for trouble.nvim]], false)
 time([[Config for rose-pine]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
 time([[Config for rose-pine]], false)
+-- Config for: remote-nvim.nvim
+time([[Config for remote-nvim.nvim]], true)
+try_loadstring("\27LJ\2\nU\0\3\v\0\4\0\t6\3\0\0'\5\1\0\18\6\0\0'\a\2\0\18\b\1\0'\t\3\0\18\n\2\0B\3\a\1K\0\1\0\16Event type:\21exited with code\vClient\nprintä\2\1\2\n\0\14\0\30'\2\0\0\18\4\2\0009\2\1\2\18\5\0\0'\6\2\0\18\b\6\0009\6\1\0069\t\3\1B\6\3\0A\2\2\0026\3\4\0009\3\5\0039\3\6\3\a\3\a\0X\3\6€'\3\b\0\18\5\3\0009\3\1\3\18\6\0\0B\3\3\2\18\2\3\0006\3\4\0009\3\t\0039\3\n\3\18\5\2\0005\6\v\0003\a\f\0=\a\r\6B\3\3\1K\0\1\0\fon_exit\0\1\0\1\vdetach\2\rjobstart\afn4kitty -e nvim --server localhost:%s --remote-ui\16xterm-kitty\tTERM\benv\bvim\thost\17'Remote: %s'\vformatgwezterm cli set-tab-title --pane-id $(wezterm cli spawn nvim --server localhost:%s --remote-ui) %s…\1\1\0\4\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0003\3\3\0=\3\5\0025\3\6\0=\3\a\2B\0\2\1K\0\1\0\17offline_mode\1\0\2\fenabled\2\14no_github\1\20client_callback\1\0\0\0\nsetup\16remote-nvim\frequire\0", "config", "remote-nvim.nvim")
+time([[Config for remote-nvim.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
