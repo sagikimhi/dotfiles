e819d9dc2d6e334df747338a591f6b5923d1b4e8
Sagi Kimhi <92639180+SagiKimhi@users.noreply.github.com>
2025-01-30 04:33:39 +0200

    maintnance(nvim): add ufo keymaps, improve diff, trouble and lsp cfg (#12)
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- nvim/lua/my_config/set.lua
+++ nvim/lua/my_config/set.lua
@@ -1,30 +1,36 @@
 vim.g.name = "value"
 
-vim.opt.backup = false
-vim.opt.swapfile = false
-vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
-vim.opt.undofile = true
-
 vim.opt.nu = true
 vim.opt.rnu = true
-vim.opt.signcolumn = "yes"
-
-vim.opt.tabstop = 4
-vim.opt.scrolloff = 10
-vim.opt.updatetime = 50
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.colorcolumn = "80"
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
 vim.opt.wrap = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.showmatch = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.smoothscroll = true
+vim.opt.termguicolors = true
+
+vim.opt.backup = true
+vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.backupdir = os.getenv("HOME") .. "/.nvim/backup"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
 vim.opt.foldlevel = 99
 vim.opt.foldcolumn = '1'
 vim.opt.foldenable = true
 vim.opt.foldlevelstart = 99
 
+vim.opt.scrolloff = 10
+vim.opt.updatetime = 50
+vim.opt.sidescrolloff = 45
+vim.opt.colorcolumn = "80"
 vim.opt.isfname:append("@-@")
-vim.opt.termguicolors = true
+vim.opt.diffopt = "diffopt=internal,filler,closeoff,algorithm:histogram,linematch:80,iblank"
