51f6ef25d760440dbeebef25f5d47cd2a09bf945
Sagi Kimhi <sagi.kim5@gmail.com>
2025-03-28 10:19:49 +0300

    chore(nvim): update neovim configurations
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -82,22 +82,22 @@ require('mason-lspconfig').setup({
             })
         end,
 
-        veridian = function()
-            local util = require('lspconfig.util')
-            require('lspconfig').veridian.setup({
-                cmd = { 'veridian' },
-                root_dir = function(fname)
-                    local filename = (
-                        (util.path.is_absolute(fname) and fname)
-                        or util.path.join(vim.loop.cwd(), fname)
-                    )
-                    return (
-                        util.root_pattern(filename) 
-                        or util.path.dirname(filename)
-                    )
-                end
-            })
-        end,
+        -- veridian = function()
+        --     local util = require('lspconfig.util')
+        --     require('lspconfig').veridian.setup({
+        --         cmd = { 'veridian' },
+        --         root_dir = function(fname)
+        --             local filename = (
+        --                 (util.path.is_absolute(fname) and fname)
+        --                 or util.path.join(vim.loop.cwd(), fname)
+        --             )
+        --             return (
+        --                 util.root_pattern(filename) 
+        --                 or util.path.dirname(filename)
+        --             )
+        --         end
+        --     })
+        -- end,
     }
 })
 
--- nvim/after/plugin/sql_ls.lua
+++ nvim/after/plugin/sql_ls.lua
@@ -1,43 +1,43 @@
-
-local util = require('lspconfig.util')
-local lspconfig = require('lspconfig')
-
-lspconfig.sqls.setup({
-    on_attach = function(client, bufnr)
-        require('sqls').on_attach(client, bufnr)
-    end,
-    filetypes = { 'sql', 'plpgsql' },
-    cmd = { "sqls", "--config", "/home/skimhi/.config/sqls/config.yml" },
-    root_dir = function(fname)
-        return require('lspconfig.util').find_git_ancestor(fname)
-        -- return require("lspconfig.util").root_pattern('.sqls.yml')
-    end,
-    single_file_support = true,
-    settings = {
-        sqls = {
-            connections = {
-                {
-                    driver = 'postgresql',
-                    dataSourceName = ''
-                        .. 'host=127.0.0.1'
-                        .. ' port=' .. os.getenv("PGPORT")
-                        .. ' user=' .. os.getenv("USER")
-                        .. ' dbname=' .. os.getenv("USER")
-                        .. ' password=' .. '' or os.getenv("PGPASSWORD"),
-                },
-            },
-        },
-    },
-})
-
-lspconfig.postgres_lsp = {
-    default_config = {
-        name = 'postgres_lsp',
-        cmd = { 'postgres_lsp' },
-    filetypes = { 'sql', 'plpgsql' },
-        single_file_support = true,
-        root_dir = require('lspconfig.util').root_pattern('root-file.txt'),
-    },
-    force_setup = true
-}
-
+--
+-- local util = require('lspconfig.util')
+-- local lspconfig = require('lspconfig')
+--
+-- lspconfig.sqls.setup({
+--     on_attach = function(client, bufnr)
+--         require('sqls').on_attach(client, bufnr)
+--     end,
+--     filetypes = { 'sql', 'plpgsql' },
+--     cmd = { "sqls", "--config", "/home/skimhi/.config/sqls/config.yml" },
+--     root_dir = function(fname)
+--         return require('lspconfig.util').find_git_ancestor(fname)
+--         -- return require("lspconfig.util").root_pattern('.sqls.yml')
+--     end,
+--     single_file_support = true,
+--     settings = {
+--         sqls = {
+--             connections = {
+--                 {
+--                     driver = 'postgresql',
+--                     dataSourceName = ''
+--                         .. 'host=127.0.0.1'
+--                         .. ' port=' .. os.getenv("PGPORT")
+--                         .. ' user=' .. os.getenv("USER")
+--                         .. ' dbname=' .. os.getenv("USER")
+--                         .. ' password=' .. '' or os.getenv("PGPASSWORD"),
+--                 },
+--             },
+--         },
+--     },
+-- })
+--
+-- lspconfig.postgres_lsp = {
+--     default_config = {
+--         name = 'postgres_lsp',
+--         cmd = { 'postgres_lsp' },
+--     filetypes = { 'sql', 'plpgsql' },
+--         single_file_support = true,
+--         root_dir = require('lspconfig.util').root_pattern('root-file.txt'),
+--     },
+--     force_setup = true
+-- }
+--
