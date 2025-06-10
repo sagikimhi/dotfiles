ec924777f2da9c30094d1ef777ede15c175428b3
Sagi Kimhi <sagi.kim5@gmail.com>
2025-06-10 08:33:33 +0300

    update(nvim): update nvim cfg
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- nvim/lua/plugins/lib/lazydev.lua
+++ nvim/lua/plugins/lib/lazydev.lua
@@ -7,6 +7,9 @@ return {
 				-- See the configuration section for more details
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				{ path = "LazyVim", words = { "LazyVim" } },
+				{ path = "snacks.nvim", words = { "Snacks" } },
+				{ path = "lazy.nvim", words = { "LazyVim" } },
 			},
 		},
 	},
